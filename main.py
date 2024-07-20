c6d638502bcd6b1b96b3535920e83e77993b451f
for-lessonn <holmonbek@gmail.com>
2024-07-20 15:38:26 +0500

    DOne

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+
+class Person:
+    def __init__(self, n, s, a):
+        self.name = n
+        self.surname = s
+        self.age = a
+
+    def work(self):
+        print("I'm working")
+
+    def info(self):
+        print(self.name, self.surname, self.age)
+
+p1 = Person("Umar", "Davlatov", 34)
+p2 = Person("Muzaffar", "A", 19)
+
+print(p2.name)
+p1.info()
+
